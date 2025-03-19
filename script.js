55c6517e72d0c8cf2b6b406561ca4d43604d5885
Tyler <tyfighter16@gmail.com>
2025-03-18 19:50:05 -0500

    Created Array's

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const str = 'This text can be anything'
+let arr = [20, 30, 40, 50, 100]
 
-for (let i = 0; i <= str.length; ++i) {
-    console.log(str[i])
-}
+// First element of array:
+console.log(arr[1])
+
+console.log(arr[arr.length - 1])
+
+let newArr = arr.filter((element) => {
+    console.log(element)
+    if (element < 50) {
+        return true;
+    }
+})
+
+console.log(newArr)
