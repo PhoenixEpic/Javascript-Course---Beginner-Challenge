0011f6ed8e8dfcc50c7f6a20616f8080c30c98a3
Tyler <tyfighter16@gmail.com>
2025-04-22 21:57:51 -0500

    Added Addition calculator, hours to seconds converter, and calculating the perimeter of a rectangle. Started calculation of the Area of a Triangle

---
+++ app.js
@@ -0,0 +1,26 @@
+//Addition calculator
+function addition(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(addition(7, 541));
+
+//Hours into Seconds converter
+const hoursIntoSeconds = (hours) => {
+    return hours * 3600
+}
+
+console.log(hoursIntoSeconds(7))
+
+//Calculate the Perimeter of a rectangle
+function calcPerimeter(length, width) {
+    return 2 * length + 2 * width;
+}
+
+console.log(calcPerimeter(6, 7))
+
+function calcAreaOfTriangle(base, height) {
+    return 
+}
+
+console.log(calcAreaOfTriangle(20,10))
