214e67492d287204b39045d7c2570b67a62a4cd0
Tyler <tyfighter16@gmail.com>
2025-04-25 14:38:56 -0500

    Continued with more JS quiz questions and answers

--- app.js
+++ app.js
@@ -19,8 +19,70 @@ function calcPerimeter(length, width) {
 
 console.log(calcPerimeter(6, 7))
 
+//Calculate the Area of a Triangle
 function calcAreaOfTriangle(base, height) {
-    return 
+    return 0.5 * base * height;
 }
 
-console.log(calcAreaOfTriangle(20,10))
+console.log(calcAreaOfTriangle(3, 2))
+
+//Add a word to the front of a static string 'AppleFrontend', etc
+function appendFrontend(addWord) {
+    return addWord + 'Frontend' 
+}
+
+console.log(appendFrontend('Orange'))
+
+//Return true if number Greater than 100 - My method
+function sumGreaterThan100(number) {
+    if (number > 100)
+        return true
+    if (number < 100)
+        return false
+}
+
+console.log(sumGreaterThan100(300))
+
+//Return true if number Greater than 100 - Answer 1
+function sumGreater2(num1, num2) {
+    if (num1 + num2 > 100) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(sumGreater2(12, 100))
+
+//Return true if number greater than 100 - Answer 2 (best practice)
+function sumGreater3(num1, num2) {
+    return num1 + num2 >= 100
+}
+
+console.log(sumGreater3(12, 300))
+
+//Less than or equal to zero
+function lessThanOrEqualToZero(num) {
+    return num <= 0
+}
+
+console.log(lessThanOrEqualToZero(-2))
+
+//Opposite boolean
+function oppositeBoolean(bool) {
+    return !bool;
+}
+
+console.log(oppositeBoolean(true))
+
+//Is not the number 0
+function isNotZero(num) {
+    if (num = 0)
+        return false
+    else {
+        return true
+    }
+}
+
+console.log(isNotZero())
